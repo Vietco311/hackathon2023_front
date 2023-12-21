73a876287550f0738e8496a8ed6da1d1076a05d8
evaFigueiredo <evafigueiredo810@gmail.com>
2023-12-21 16:55:33 +0100

    css

--- hackahon2023_front/src/App.js
+++ hackahon2023_front/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import './App.css';
+import ChatBot from 'react-simple-chatbot';
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const [viewVisible, setViewVisible] = useState(false);
+
   function setNewMessage(msg) {
       setMessages([
         ...messages,
@@ -17,13 +21,18 @@ function App() {
       };
       setNewMessage(msg);
     }
+
+    const toggleView = () => {
+      setViewVisible(!viewVisible);
+    };
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-4">
             <div className="card">
               <div className="card-body">
-                <div className="card-title">My first chat</div>
+                <div className="card-title">Chat</div>
                 <hr/>
                 <div className="messages">
                   {messages.map(msg => {
@@ -33,26 +42,28 @@ function App() {
                   })}
                 </div>
               </div>
-              <form onSubmit={e => sendMessage(e)}>
-                <div className="card-footer">
-                  <input id="username"
-                         type="text"
-                         placeholder="Username"
-                         className="form-control"
-                  />
-                  <br/>
-                  <input id="text"
-                         type="text"
-                         placeholder="Your message"
-                         className="form-control"
-                  />
-                  <br/>
-                  <button type="submit"
-                          className="btn btn-primary form-control">
-                    send
-                  </button>
+              {viewVisible && (
+                <div id="view" className="visible">
+                  <form onSubmit={e => sendMessage(e)}>
+                  <div className="card-footer">
+                    <input id="username"
+                          type="text"
+                          placeholder="Nom"
+                          className="form-control"
+                    />
+                    <br/>
+                    <input id="text"
+                          type="text"
+                          placeholder="Votre message"
+                          className="form-control"
+                    />
+                    <br/>
+                    <button type="submit">Envoyer </button>
+                  </div>
+                </form>
                 </div>
-              </form>
+              )}
+              <button className="chat-button" onClick={toggleView}><img src="logoChatNB.svg" alt="Chat logo" /> </button>
             </div>
           </div>
         </div>
