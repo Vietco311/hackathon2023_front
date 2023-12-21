9a7ba2422bbc5f66ab5aa79aa2f9ef1cec46a504
Vietco311 <ant.colin311@gmail.com>
2023-12-21 16:56:30 +0100

    recup data

--- hackahon2023_front/src/App.js
+++ hackahon2023_front/src/App.js
@@ -1,39 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ChatBot from 'react-simple-chatbot';
+
 
 function App() {
+  const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
-  function setNewMessage(msg) {
-      setMessages([
-        ...messages,
-        msg
-      ]);
-    }
 
-    function sendMessage(e) {
-      e.preventDefault();
-      const msg = {
-        username: e.target.username.value,
-        text: e.target.text.value
-      };
-      setNewMessage(msg);
+const chatWithBot = async (userInput) => {
+    const apiEndpoint = 'https://hackathon-2023-api.carlotti-toussaint.com/api/chat';
+
+    const data = {
+      prompt: userInput,
+      max_tokens: 150
+    };
+try {
+      const response = await axios.post(apiEndpoint, data, { withCredentials: true });
+      return response.data.choices[0].text.trim();
+    } catch (error) {
+      console.error('Error communicating with the API:', error.message);
+      return '';
     }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    const userMessage = { text: input, user: true };
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    const aiMessage = { text: '...', user: false };
+    setMessages((prevMessages) => [...prevMessages, aiMessage]);
+    const response = await chatWithBot(input);
+    const newAiMessage = { text: response, user: false };
+    setMessages((prevMessages) => [...prevMessages.slice(0, -1), newAiMessage]);
+    setInput('');
+  }
     return (
+      
+      
       <div className="container">
         <div className="row">
           <div className="col-4">
             <div className="card">
               <div className="card-body">
+              <div>
+              </div>
+              
                 <div className="card-title">My first chat</div>
                 <hr/>
-                <div className="messages">
-                  {messages.map(msg => {
+                {<div className="messages">
+                  {messages.map((msg,index) => {
                     return (
-                        <div>{msg.username}: {msg.text}</div>
+                        <div key={index}>{msg.role}: {msg.content}</div>
                     )
                   })}
-                </div>
+                </div>}
               </div>
-              <form onSubmit={e => sendMessage(e)}>
+              
+              <form onSubmit={e => handleSubmit(e)}>
                 <div className="card-footer">
                   <input id="username"
                          type="text"
